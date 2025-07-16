44a546c8d16a0717e51c99b188264c8d2058c385
gowtham01raj <gowthamraj.ts@outlook.com>
2025-07-16 15:12:20 +0530

    first

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
 import { addFeed } from "../utils/feedSlice";
